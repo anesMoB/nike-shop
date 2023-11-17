b20cf11cfe452a8dbf356dbcc7246068c3337fc0
anesMoB <83066640+anesMoB@users.noreply.github.com>
2023-11-17 11:55:04 +0100

    Update CustomerReviewCard.tsx

--- app/componants/CustomerReviewCard.tsx
+++ app/componants/CustomerReviewCard.tsx
@@ -17,7 +17,7 @@ const CustomerReviewCard = ({reviewerImg,reviewerName,reviewerDescription,review
          <Image className='rounded-full object-cover' width={150} height={150} src={reviewerImg} alt={'image'} />
       <p className='text-gray-500 leading-8 text-lg lg:max-w-sm'>{reviewerDescription}</p>
       <div className='flex justify-center items-center gap-4'>
-        <Image width={20} height={20} src={'/assets/icons/star.svg'} alt={'image'}/> 
+        <Image width={20} height={20} src={'./assets/icons/star.svg'} alt={'image'}/> 
         <h1 className='text-gray-500 leading-8'>({reviewerRating})</h1>
       </div>
       <h1 className='text-gray-800 leading-8 font-bold text-2xl'>{reviewerName}</h1>
